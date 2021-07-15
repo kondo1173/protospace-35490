3fafe406e35df204c3de1326d329164cac15cd7d
kondo1173 <migumeudonko@icloud.com>
2021-07-15 11:12:07 +0900

    edit画面の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -28,13 +28,14 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-  @prototype = Prototype.find(params[:id])
+   @prototype = Prototype.find(params[:id])
   end
 
+
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
-      redirect_to prototype_path(prototype.id)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
     else
       render :edit
     end
